5a39239396e1cd4991f22b5ef36c16ddc08fc916
Daeram Chung <daeram.chung@gmail.com>
2023-07-10 19:26:00 +0900

    #1 🔧chore: 디자인 시스템 설정
    - 폰트 CDN
    - CSS reset
    - 디자인 시스템
    - 아이콘 파일 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,37 @@
 import React from "react";
-import logo from "./logo.svg";
+import styled, { ThemeProvider } from "styled-components";
+import designSystem from "./styles/design-system.ts";
+import GlobalStyles from "./styles/GlobalStyles.ts";
 import "./App.css";
+import logo from "./logo.svg";
+
+const Title = styled.h1`
+  color: ${({ theme: { colors } }) => colors.blue};
+  font: ${({ theme: { font } }) => font.displayMD16};
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={designSystem}>
+      <GlobalStyles />
+
+      <div className="App">
+        <header className="App-header">
+          <Title>Styled Components</Title>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
