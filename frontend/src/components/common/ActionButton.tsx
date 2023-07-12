8fd29f25080469c9f92d1f11dbaa897b94fcb8ea
Daeram Chung <daeram.chung@gmail.com>
2023-07-13 02:02:55 +0900

    #2 💄 design: ActionButton 버튼 타입 및 disabled 여부 추가
    - 추후에 optional한 click event listener도 추가 예정.

--- frontend/src/components/common/ActionButton.tsx
+++ frontend/src/components/common/ActionButton.tsx
@@ -4,14 +4,24 @@ import { styled } from "styled-components";
 export default function ActionButton({
   className,
   content,
+  type,
+  disabled,
 }: {
   className: string;
   content: string;
+  type: "button" | "submit" | "reset";
+  disabled?: boolean;
 }) {
   return (
-    <StyledActionButton className={className}>{content}</StyledActionButton>
+    <StyledActionButton {...{ className, type, disabled }}>
+      {content}
+    </StyledActionButton>
   );
 }
+
+ActionButton.defaultProps = {
+  disabled: false,
+};
 
 const StyledActionButton = styled.button`
   width: 132px;
@@ -28,7 +38,8 @@ const StyledActionButton = styled.button`
     color: ${({ theme: { colors } }) => colors.grey600};
   }
 
-  &.add-button {
+  &.add-button,
+  &.edit-button {
     background-color: ${({ theme: { colors } }) => colors.blue};
     color: ${({ theme: { colors } }) => colors.grey50};
   }
@@ -36,5 +47,10 @@ const StyledActionButton = styled.button`
   &.delete-button {
     background-color: ${({ theme: { colors } }) => colors.red};
     color: ${({ theme: { colors } }) => colors.grey50};
+  }
+
+  &:disabled {
+    opacity: ${({ theme: { opacity } }) => opacity.disabled};
+    cursor: not-allowed;
   }
 `;
