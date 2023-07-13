e3f6a330d6ce5eec4a403785095cad668b32dad5
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-07-13 14:31:48 +0900

    # ✨9 #10 #11  feat : 서비스에 카드 등록, 수정, 삭제 메소드 구현

--- be/src/main/java/codesquad/todo/card/service/CardService.java
+++ be/src/main/java/codesquad/todo/card/service/CardService.java
@@ -2,6 +2,11 @@ package codesquad.todo.card.service;
 
 import org.springframework.stereotype.Service;
 
+import codesquad.todo.card.controller.dto.CardDeleteResponse;
+import codesquad.todo.card.controller.dto.CardModifyRequest;
+import codesquad.todo.card.controller.dto.CardModifyResponse;
+import codesquad.todo.card.controller.dto.CardSaveRequest;
+import codesquad.todo.card.controller.dto.CardSaveResponse;
 import codesquad.todo.card.repository.CardRepository;
 
 @Service
@@ -12,4 +17,17 @@ public class CardService {
 	public CardService(CardRepository cardRepository) {
 		this.cardRepository = cardRepository;
 	}
+
+	public CardSaveResponse saveCard(CardSaveRequest cardSaveRequest) {
+		return CardSaveResponse.from(cardRepository.save(cardSaveRequest.toEntity()));
+	}
+
+	public CardModifyResponse modifyCard(CardModifyRequest cardModifyRequest) {
+		return CardModifyResponse.from(cardRepository.modify(cardModifyRequest.toEntity()));
+	}
+
+	public CardDeleteResponse deleteCard(Long cardId) {
+		return CardDeleteResponse.from(cardRepository.deleteById(cardId));
+	}
+
 }
