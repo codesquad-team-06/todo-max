a7be948fb87ddd0d138d392c261513611dc71bd0
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-07-13 14:32:41 +0900

    #9 #10 #11 ✨ feat : 컨트롤러 등록, 수정, 삭제 매핑 구현

--- be/src/main/java/codesquad/todo/card/controller/CardRestController.java
+++ be/src/main/java/codesquad/todo/card/controller/CardRestController.java
@@ -1,14 +1,43 @@
 package codesquad.todo.card.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import codesquad.todo.card.controller.dto.CardDeleteResponse;
+import codesquad.todo.card.controller.dto.CardModifyRequest;
+import codesquad.todo.card.controller.dto.CardModifyResponse;
+import codesquad.todo.card.controller.dto.CardSaveRequest;
+import codesquad.todo.card.controller.dto.CardSaveResponse;
 import codesquad.todo.card.service.CardService;
 
 @RestController
+@RequestMapping("/cards")
 public class CardRestController {
 	private final CardService cardService;
 
+	@Autowired
 	public CardRestController(CardService cardService) {
 		this.cardService = cardService;
+	}
+
+	@PostMapping
+	public CardSaveResponse saveCard(@RequestBody CardSaveRequest cardSaveRequest) {
+		return cardService.saveCard(cardSaveRequest);
+	}
+
+	@PutMapping
+	public CardModifyResponse modifyCard(@RequestBody CardModifyRequest cardModifyRequest) {
+		return cardService.modifyCard(cardModifyRequest);
+	}
+
+	@DeleteMapping("/{cardId}")
+	public CardDeleteResponse deleteCard(@PathVariable Long cardId) {
+		return cardService.deleteCard(cardId);
 	}
 }
