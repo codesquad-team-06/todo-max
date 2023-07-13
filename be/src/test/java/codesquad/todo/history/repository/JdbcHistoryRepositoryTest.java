c28ef3b83fc004d83782d5d9f096ff029e660830
O_jeegu <lumirite0@gmail.com>
2023-07-14 04:32:51 +0900

    #5 ✅ test : 활동 기록을 삭제하는 Test 코드 작성

--- be/src/test/java/codesquad/todo/history/repository/JdbcHistoryRepositoryTest.java
+++ be/src/test/java/codesquad/todo/history/repository/JdbcHistoryRepositoryTest.java
@@ -1,5 +1,7 @@
 package codesquad.todo.history.repository;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.assertj.core.api.SoftAssertions;
@@ -32,6 +34,36 @@ class JdbcHistoryRepositoryTest {
 		//then
 		SoftAssertions.assertSoftly(softAssertions -> {
 			softAssertions.assertThat(historyList.size()).isEqualTo(7);
+		});
+	}
+
+	@Test
+	@DisplayName("전달 받은 활동 기록 아이디가 모두 유효한 지 확인한다.")
+	public void testCountIds() {
+		//given
+		List<Long> ids = new ArrayList<>(Arrays.asList(4L, 5L, 6L));
+
+		//when
+		int validIds = historyRepository.countByIds(ids);
+
+		//then
+		SoftAssertions.assertSoftly(softAssertions -> {
+			softAssertions.assertThat(validIds).isEqualTo(3);
+		});
+	}
+
+	@Test
+	@DisplayName("활동 기록 아이디에 따라 해당 기록의 is_deleted 값을 false로 변경하고 변경된 레코드 수를 반환한다.")
+	public void testDeleteByIds() {
+		//given
+		List<Long> ids = new ArrayList<>(Arrays.asList(4L, 5L, 6L));
+
+		//when
+		int deletedHistories = historyRepository.deleteByIds(ids);
+
+		//then
+		SoftAssertions.assertSoftly(softAssertions -> {
+			softAssertions.assertThat(deletedHistories).isEqualTo(3);
 		});
 	}
 }
