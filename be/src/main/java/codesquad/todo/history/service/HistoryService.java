5e1988cb3078f39bf5154d0073009dd972662dc8
O_jeegu <lumirite0@gmail.com>
2023-07-13 12:23:47 +0900

    #5,7 ✨ feat : 히스토리 전체 조회 및 삭제 API 구현

--- be/src/main/java/codesquad/todo/history/service/HistoryService.java
+++ be/src/main/java/codesquad/todo/history/service/HistoryService.java
@@ -1,7 +1,12 @@
 package codesquad.todo.history.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import codesquad.todo.history.controller.dto.HistoryFindAllResponse;
 import codesquad.todo.history.repository.HistoryRepository;
 
 @Service
@@ -11,5 +16,28 @@ public class HistoryService {
 
 	public HistoryService(HistoryRepository historyRepository) {
 		this.historyRepository = historyRepository;
+	}
+
+	@Transactional(readOnly = true)
+	public List<HistoryFindAllResponse> getAll() {
+		return historyRepository.findAll()
+			.stream()
+			.map(HistoryFindAllResponse::from)
+			.collect(Collectors.toList());
+	}
+
+	@Transactional
+	public boolean deleteAndFetch(List<Long> ids) {
+		validate(ids);
+		int deletedRows = historyRepository.deleteByIds(ids);
+		return deletedRows > 0;
+	}
+
+	//todo : 예외처리 ENUM
+	public void validate(List<Long> ids) {
+		int count = historyRepository.countByIds(ids);
+		if (count != ids.size()) {
+			throw new RuntimeException("유효하지 않은 히스토리입니다.");
+		}
 	}
 }
