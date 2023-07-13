a28564929a094f31b47dde03bb36fcd7503e1159
O_jeegu <lumirite0@gmail.com>
2023-07-14 04:16:59 +0900

    #5 ✅ test : 유효한 활동 목록을 불러오는 Test 코드 작성

--- be/src/test/java/codesquad/todo/history/repository/JdbcHistoryRepositoryTest.java
+++ be/src/test/java/codesquad/todo/history/repository/JdbcHistoryRepositoryTest.java
@@ -1,5 +1,37 @@
 package codesquad.todo.history.repository;
 
-class JdbcHistoryRepositoryTest {
+import java.util.List;
 
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Repository;
+
+import codesquad.todo.history.entity.History;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION,
+	classes = Repository.class))
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class JdbcHistoryRepositoryTest {
+	@Autowired
+	private HistoryRepository historyRepository;
+
+	@Test
+	@DisplayName("is_deleted = false인 모든 활동 기록을 불러온다.")
+	public void testFindAll() {
+		//given
+
+		//when
+		List<History> historyList = historyRepository.findAll();
+
+		//then
+		SoftAssertions.assertSoftly(softAssertions -> {
+			softAssertions.assertThat(historyList.size()).isEqualTo(7);
+		});
+	}
 }
