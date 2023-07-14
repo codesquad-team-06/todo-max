1d720ae5865a48021b9e56c4521d1090d5b0990d
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-07-14 12:51:28 +0900

    #36 🐛fix: 테스트 코드 오타 수정

--- be/src/test/java/codesquad/todo/card/repository/JdbcCardRepositoryTest.java
+++ be/src/test/java/codesquad/todo/card/repository/JdbcCardRepositoryTest.java
@@ -1,9 +1,7 @@
 package codesquad.todo.card.repository;
 
-
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
-
 
 import java.util.List;
 
@@ -17,16 +15,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Repository;
 
-import org.springframework.transaction.annotation.Transactional;
-
-
 import codesquad.todo.card.entity.Card;
 
 // Repository 애노테이션이 붙은 클래스만 빈으로 등록
 @DataJpaTest(includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Repository.class))
 // Replace.NONE으로 설정하면 @ActiveProfiles에 설정한 프로파일 환경값에 따라 데이터소스가 적용된다.
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-
 class JdbcCardRepositoryTest {
 
 	@Autowired
@@ -112,7 +106,8 @@ class JdbcCardRepositoryTest {
 			() -> assertThat(card.getPosition()).isEqualTo(2048),
 			() -> assertThat(card.isDeleted()).isFalse()
 		);
-    
+	}
+
 	@DisplayName("모든 카드 데이터를 요청합니다.")
 	public void testFindAll() {
 		// given
