79c4f5dda45d72533d9350f45f522009c80d9286
tmdqja0705@naver.com <tmdqja0705@naver.com>
2023-07-18 16:24:05 +0900

    #55 ✨feat: 카드 생성, 수정 유효성 검증 구현

--- be/src/main/java/codesquad/todo/card/controller/dto/CardSaveRequest.java
+++ be/src/main/java/codesquad/todo/card/controller/dto/CardSaveRequest.java
@@ -1,16 +1,23 @@
 package codesquad.todo.card.controller.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 import codesquad.todo.card.entity.Card;
 
 public class CardSaveRequest {
+	@NotEmpty(message = "카드 제목을 입력해주세요.")
+	@Size(max = 50, message = "글자 수를 초과했습니다. 카드 내용을 50자 이하로 입력해주세요.")
 	String title;
+	@NotEmpty(message = "카드 내용을 입력해주세요.")
+	@Size(max = 500, message = "글자 수를 초과했습니다. 카드 내용을 500자 이하로 입력해주세요.")
 	String content;
-	long columnId;
+	Long columnId;
 
 	public CardSaveRequest() {
 	}
 
-	public CardSaveRequest(String title, String content, long columnId) {
+	public CardSaveRequest(String title, String content, Long columnId) {
 		this.title = title;
 		this.content = content;
 		this.columnId = columnId;
@@ -24,7 +31,7 @@ public class CardSaveRequest {
 		return content;
 	}
 
-	public long getColumnId() {
+	public Long getColumnId() {
 		return columnId;
 	}
 
