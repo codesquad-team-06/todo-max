35c26f1b1d552044473ca13502e9522ad4e92293
Daeram Chung <daeram.chung@gmail.com>
2023-07-18 15:01:11 +0900

    #53 ✨feat: Drag & Drop mouse down/move 작업 초기 세팅
    - Drag & Drop 관련 states 초기화.
    - Board -> Column -> ColumnCard로 props 전달.
    - `ColumnCard`에서 `useRef`로 현재 카드를 저장하여 styled components에서 width, height 접근.

--- frontend/src/components/ColumnCard/ColumnCard.tsx
+++ frontend/src/components/ColumnCard/ColumnCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, MouseEvent } from "react";
 import { styled } from "styled-components";
 import ColumnCardDisplay from "./ColumnCardDisplay.tsx";
 import ColumnCardMode from "./ColumnCardMode.tsx";
@@ -6,28 +6,53 @@ import { Card } from "../../types.ts";
 
 export default function ColumnCard({
   cardDetails,
+  currMouseCoords,
+  isDraggingCardId,
   editCardHandler,
   deleteCardHandler,
+  isDraggingCardIdHandler,
 }: {
   cardDetails: {
     id: number;
     title: string;
     content: string;
   };
+  currMouseCoords: [number, number];
+  isDraggingCardId: number | null;
   editCardHandler: (card: Card) => void;
   deleteCardHandler: (deletedCardInfo: {
     id: number;
     columnId: number;
   }) => void;
+  isDraggingCardIdHandler: (cardId: number | null) => void;
 }) {
   const [isEditMode, setIsEditMode] = useState<boolean>(false);
+  const columnCardRef = useRef<HTMLLIElement | null>(null);
 
   const toggleEditMode = () => {
     setIsEditMode(!isEditMode);
   };
 
+  const mouseDownHandler = (evt: MouseEvent) => {
+    // Dragging 시작
+    isDraggingCardIdHandler(cardDetails.id);
+    // 그랩된 카드는 absolute로 마우스 따라서 드래그 중
+
+    // 마우스 위치에 따라 잔상 위치 옮기기
+  };
+
+  const mouseUpHandler = (evt: MouseEvent) => {
+    isDraggingCardIdHandler(cardDetails.id);
+  };
+
   return (
-    <StyledColumnCard>
+    <StyledColumnCard
+      ref={columnCardRef}
+      $currentCardRef={columnCardRef}
+      $isGrabbed={isDraggingCardId === cardDetails.id}
+      $currMouseCoords={currMouseCoords}
+      onMouseDown={mouseDownHandler}
+      onMouseUp={mouseUpHandler}>
       {isEditMode ? (
         <ColumnCardMode
           {...{
@@ -50,12 +75,29 @@ export default function ColumnCard({
   );
 }
 
-const StyledColumnCard = styled.li`
-  width: 100%;
+const StyledColumnCard = styled.li<{
+  $currentCardRef: React.RefObject<HTMLLIElement | null>;
+  $isGrabbed: boolean;
+  $currMouseCoords: [number, number];
+}>`
+  width: inherit;
   min-height: 104px;
   padding: 16px;
+  position: ${({ $isGrabbed }) => ($isGrabbed ? "absolute" : "static")};
+  top: ${({ $currMouseCoords, $currentCardRef }) => {
+    const columnCardHeight =
+      $currentCardRef.current?.getBoundingClientRect().height ?? 0;
+    return `${$currMouseCoords[1] - columnCardHeight / 2}px;`;
+  }};
+  left: ${({ $currMouseCoords, $currentCardRef }) => {
+    const columnCardWidth =
+      $currentCardRef.current?.getBoundingClientRect().width ?? 0;
+    return `${$currMouseCoords[0] - columnCardWidth / 2}px;`;
+  }};
   background-color: ${({ theme: { colors } }) => colors.grey50};
   border-radius: ${({ theme: { objectStyles } }) => objectStyles.radius.s};
   box-shadow: ${({ theme: { objectStyles } }) =>
     objectStyles.dropShadow.normal};
+  cursor: ${({ $isGrabbed }) => ($isGrabbed ? "grabbing" : "grab")};
+  user-select: none;
 `;
