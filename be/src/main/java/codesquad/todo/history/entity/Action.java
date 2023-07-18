2fdafce2c7494145dd6274890d0a402d94ba917a
O_jeegu <lumirite0@gmail.com>
2023-07-18 16:35:08 +0900

    #50 ✨feat: Action enum 생성
    기존 Action 클래스를 삭제하고 enum 클래스 생성

--- be/src/main/java/codesquad/todo/history/entity/Action.java
+++ be/src/main/java/codesquad/todo/history/entity/Action.java
@@ -1,53 +1,18 @@
 package codesquad.todo.history.entity;
 
-public class Action {
-	private Long id;
-	private String name;
+public enum Action {
+	REGISTERED("등록"),
+	DELETED("삭제"),
+	MOVED("이동"),
+	MODIFIED("수정");
 
-	public Action() {
-	}
+	private final String name;
 
-	public Action(Long id, String name) {
-		this.id = id;
+	Action(String name) {
 		this.name = name;
-	}
-
-	public static Builder builder() {
-		return new Builder();
-	}
-
-	public Long getId() {
-		return id;
 	}
 
 	public String getName() {
 		return name;
-	}
-
-	@Override
-	public String toString() {
-		return "Action{"
-			+ "id=" + id
-			+ ", name='" + name + '\''
-			+ '}';
-	}
-
-	public static class Builder {
-		private Long id;
-		private String name;
-
-		public Builder id(Long id) {
-			this.id = id;
-			return this;
-		}
-
-		public Builder name(String name) {
-			this.name = name;
-			return this;
-		}
-
-		public Action build() {
-			return new Action(id, name);
-		}
 	}
 }
