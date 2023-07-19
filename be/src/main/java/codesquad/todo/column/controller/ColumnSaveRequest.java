a22279b1b0fab4e09a4d2895536e73fe060af70e
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-07-19 11:46:33 +0900

    - 일관된 유효성 검사를 위하여 변경하였습니다.

--- be/src/main/java/codesquad/todo/column/controller/ColumnSaveRequest.java
+++ be/src/main/java/codesquad/todo/column/controller/ColumnSaveRequest.java
@@ -1,14 +1,13 @@
 package codesquad.todo.column.controller;
 
 import javax.validation.constraints.NotEmpty;
-
-import org.hibernate.validator.constraints.Length;
+import javax.validation.constraints.Size;
 
 import codesquad.todo.column.entity.Column;
 
 public class ColumnSaveRequest {
 	@NotEmpty(message = "컬럼의 제목은 공백이면 안됩니다.")
-	@Length(max = 100, message = "컬럼의 제목은 최대 100글자 이내여야 합니다.")
+	@Size(max = 100, message = "컬럼의 제목은 최대 100글자 이내여야 합니다.")
 	private String name;
 
 	public ColumnSaveRequest() {
