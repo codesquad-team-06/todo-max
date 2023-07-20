65c01f0f0d18cab87a57921908ad9d0c188c1722
O_jeegu <lumirite0@gmail.com>
2023-07-20 22:41:29 +0900

    #83 ✅test: 테스트코드 수정

--- be/src/test/java/codesquad/todo/aspect/CardHistoryLogAspectTest.java
+++ be/src/test/java/codesquad/todo/aspect/CardHistoryLogAspectTest.java
@@ -1,14 +1,12 @@
 package codesquad.todo.aspect;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,8 +14,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import codesquad.todo.card.controller.dto.CardDeleteResponse;
+import codesquad.todo.card.controller.dto.CardModifyResponse;
 import codesquad.todo.card.controller.dto.CardMoveRequest;
+import codesquad.todo.card.controller.dto.CardMoveResponse;
+import codesquad.todo.card.controller.dto.CardSaveResponse;
 import codesquad.todo.card.entity.Card;
+import codesquad.todo.card.repository.CardRepository;
 import codesquad.todo.column.repository.ColumnRepository;
 import codesquad.todo.history.controller.dto.HistorySaveDto;
 import codesquad.todo.history.service.HistoryService;
@@ -26,86 +29,87 @@ import codesquad.todo.history.service.HistoryService;
 public class CardHistoryLogAspectTest {
 
 	@Mock
-	ProceedingJoinPoint proceedingJoinPoint;
-	@Mock
-	JoinPoint joinPoint;
-	@Mock
-	MethodSignature methodSignature;
-	@Mock
-	Card card;
-	@InjectMocks
-	private CardHistoryLogAspect aspect;
-	@Mock
 	private HistoryService historyService;
+
+	@Mock
+	private CardRepository cardRepository;
+
 	@Mock
 	private ColumnRepository columnRepository;
 
-	@Test
-	@DisplayName("logForMove를 실행하면 historyService.save()가 1번 호출된다.")
-	public void logForMoveTest() throws Throwable {
-		//given
-		CardMoveRequest request = mock(CardMoveRequest.class);
-		when(proceedingJoinPoint.getArgs()).thenReturn(new Object[] {request});
-		when(proceedingJoinPoint.getSignature()).thenReturn(methodSignature);
-		when(methodSignature.getName()).thenReturn("moveCard");
-		when(proceedingJoinPoint.proceed()).thenReturn(card);
-		List<String> columnNames = Arrays.asList("Column1", "Column2");
-		when(columnRepository.findAllNameById(anyList())).thenReturn(columnNames);
+	@Mock
+	private Card card;
 
-		//when
-		aspect.logForMove(proceedingJoinPoint);
+	@Mock
+	private ProceedingJoinPoint proceedingJoinPoint;
 
-		//then
-		verify(historyService, times(1)).save(any(HistorySaveDto.class));
-	}
+	@InjectMocks
+	private CardHistoryLogAspect cardHistoryLogAspect;
 
 	@Test
-	@DisplayName("logForOthers를 실행하면 historyService.save()가 1번 호출된다.")
-	public void logForSaveTest() {
+	@DisplayName("logForSave()를 실행하면 historyService.save() 메서드가 1번 호출된다.")
+	public void testLogForSave() {
 		//given
-		when(joinPoint.getSignature()).thenReturn(methodSignature);
-		when(methodSignature.getName()).thenReturn("saveCard");
+		CardSaveResponse cardSaveResponse = CardSaveResponse.from(card);
 		List<String> columnNames = Arrays.asList("Column1");
 		when(columnRepository.findAllNameById(anyList())).thenReturn(columnNames);
 
 		//when
-		aspect.logForOthers(joinPoint, card);
+		cardHistoryLogAspect.logForSave(cardSaveResponse);
 
 		//then
 		verify(historyService, times(1)).save(any(HistorySaveDto.class));
 	}
 
 	@Test
-	@DisplayName("logForMove 실행 시 매치하는 Action 값이 없으면 예외를 발생하고 historyService.save 메서드를 호출하지 않는다.")
-	public void logForMove_ThrowExceptionTest() throws Throwable {
+	@DisplayName("logForModify()를 실행하면 historyService.save() 메서드가 1번 호출된다.")
+	public void testLogForModify() {
 		//given
-		CardMoveRequest request = mock(CardMoveRequest.class);
-		when(proceedingJoinPoint.getArgs()).thenReturn(new Object[] {request});
-		when(proceedingJoinPoint.getSignature()).thenReturn(methodSignature);
-		when(methodSignature.getName()).thenReturn("movCard");
-		when(proceedingJoinPoint.proceed()).thenReturn(card);
+		CardModifyResponse cardModifyResponse = CardModifyResponse.from(card);
+		List<String> columnNames = Arrays.asList("Column1");
+		when(columnRepository.findAllNameById(anyList())).thenReturn(columnNames);
 
 		//when
-		Exception exception = assertThrows(IllegalArgumentException.class,
-			() -> aspect.logForMove(proceedingJoinPoint));
+		cardHistoryLogAspect.logForModify(cardModifyResponse);
 
 		//then
-		assertEquals(exception.getMessage(), "Invalid method name: movCard");
-		verify(historyService, times(0)).save(any(HistorySaveDto.class));
+		verify(historyService, times(1)).save(any(HistorySaveDto.class));
 	}
 
 	@Test
-	@DisplayName("logForOthers 실행 시 매치하는 Action 값이 없으면 예외를 발생하고 historyService.save 메서드를 호출하지 않는다.")
-	public void logForDelete_ThrowExceptionTest() {
+	@DisplayName("logForDelete()를 실행하면 historyService.save() 메서드가 1번 호출된다.")
+	public void testLogForDelete() throws Throwable {
 		//given
-		when(joinPoint.getSignature()).thenReturn(methodSignature);
-		when(methodSignature.getName()).thenReturn("delete");
+		Long cardId = 1L;
+		CardDeleteResponse cardDeleteResponse = new CardDeleteResponse(cardId, true);
+		List<String> columnNames = Arrays.asList("Column1");
+		when(columnRepository.findAllNameById(anyList())).thenReturn(columnNames);
+		when(proceedingJoinPoint.getArgs()).thenReturn(new Object[] {cardId});
+		when(cardRepository.findById(cardId)).thenReturn(card);
+		when(proceedingJoinPoint.proceed()).thenReturn(cardDeleteResponse);
 
 		//when
-		Exception exception = assertThrows(IllegalArgumentException.class, () -> aspect.logForOthers(joinPoint, card));
+		cardHistoryLogAspect.logForDelete(proceedingJoinPoint);
 
 		//then
-		assertEquals(exception.getMessage(), "Invalid method name: delete");
-		verify(historyService, times(0)).save(any(HistorySaveDto.class));
+		verify(historyService, times(1)).save(any(HistorySaveDto.class));
+	}
+
+	@Test
+	@DisplayName("logForMove()를 실행하면 historyService.save() 메서드가 1번 호출된다.")
+	public void testLogForMove() throws Throwable {
+		//given
+		CardMoveRequest request = mock(CardMoveRequest.class);
+		when(proceedingJoinPoint.getArgs()).thenReturn(new Object[] {request});
+		CardMoveResponse cardMoveResponse = CardMoveResponse.from(card);
+		when(proceedingJoinPoint.proceed()).thenReturn(cardMoveResponse);
+		List<String> columnNames = Arrays.asList("Column1", "Column2");
+		when(columnRepository.findAllNameById(anyList())).thenReturn(columnNames);
+
+		//when
+		cardHistoryLogAspect.logForMove(proceedingJoinPoint);
+
+		//then
+		verify(historyService, times(1)).save(any(HistorySaveDto.class));
 	}
 }
