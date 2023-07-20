1d803b89ee6e34f2e3a07dbba448cf79c82e6f10
O_jeegu <lumirite0@gmail.com>
2023-07-20 22:02:46 +0900

    #83 ♻refactor: 카드 액션 메서드별 aop 기능 분리

--- be/src/main/java/codesquad/todo/aspect/PointCuts.java
+++ be/src/main/java/codesquad/todo/aspect/PointCuts.java
@@ -3,15 +3,24 @@ package codesquad.todo.aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class PointCuts {
-	@Pointcut("execution(public * codesquad.todo.card.service.CardService.moveCard(..))")
-	public void moveAction() {
+
+	@Pointcut("execution(public * codesquad.todo.card.service.CardService.saveCard(..))")
+	public void saveAction() {
 
 	}
 
-	@Pointcut("execution(public * codesquad.todo.card.service.CardService.saveCard(..)) || "
-		+ "execution(public * codesquad.todo.card.service.CardService.deleteCard(..)) || "
-		+ "execution(public * codesquad.todo.card.service.CardService.modifyCard(..))")
-	public void otherActions() {
+	@Pointcut("execution(public * codesquad.todo.card.service.CardService.deleteCard(..))")
+	public void deleteAction() {
+
+	}
+
+	@Pointcut("execution(public * codesquad.todo.card.service.CardService.modifyCard(..))")
+	public void modifyAction() {
+
+	}
+
+	@Pointcut("execution(public * codesquad.todo.card.service.CardService.moveCard(..))")
+	public void moveAction() {
 
 	}
 }
